bf1f9b6dccb27c7f256e9322f0358588f9c014f8
Wriruri <ulyashich@yandex.ru>
2024-06-09 14:00:42 +0300

    Initial commit

---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //private int speed=29;
+    //public float carSP;
+    //public double carcarSP;
+    //public bool carcarcarSP;
+    //public string carcarSPName;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector3(0, 0, 1)*Time.deltaTime*speed);
+    }
+}
